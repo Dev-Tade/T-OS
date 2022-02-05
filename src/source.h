0cc88c8749ee34107975637b81d53f4c923b9367
TadCodesG <88458312+TadCodesG@users.noreply.github.com>
2022-02-05 10:14:51 -0300

    Add files via upload

---
+++ src/source.h
@@ -0,0 +1,19 @@
+#ifndef SOURCE_DOT_H
+#define SOURCE_DOT_H
+
+#define VGA_ADDRESS 0xB8000
+
+#define BLACK 0
+#define GREEN 2
+#define RED 4
+#define YELLOW 14
+#define WHITE_COLOR 15
+
+unsigned short* terminal_buffer;
+unsigned int vga_index;
+
+void print_string(char* str, unsigned char color);
+void print_char(char str, unsigned char color);
+void clear_screen(void);
+
+#endif
